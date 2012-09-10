1d28d8e8ac04c05dbdfd384db74a965e33e497d6
Danny <dh.programming@havenith.net>
2012-09-11 00:18:39 +0200

    Fixing doctype headers, solving IE rendering issues

--- burndown_chart.php
+++ burndown_chart.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <?php
 	$sprint_id = $_GET['sprint_id']; 
 ?>
--- team_member.php
+++ team_member.php
@@ -3,7 +3,7 @@ require_once 'get_username.inc.php';
 require_once 'get_sprint_id.inc.php';
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
