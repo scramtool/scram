845936acb0e12e5b09f954a3be24ce2d9424b205
danny@havenith.net <dh.git@havenith.net>
2013-07-16 13:46:18 +0200

    Update database structure.

--- scram_db_structure.sql
+++ scram_db_structure.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2013 at 04:12 PM
+-- Generation Time: Jul 16, 2013 at 01:45 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `scram`
@@ -40,7 +34,8 @@ CREATE TABLE IF NOT EXISTS `availability` (
 -- Stand-in structure for view `extended_log`
 --
 CREATE TABLE IF NOT EXISTS `extended_log` (
-`type` enum('estimate','move','update')
+`time` datetime
+,`type` enum('estimate','move','update')
 ,`details` varchar(32)
 ,`name` varchar(50)
 ,`description` varchar(64)
@@ -72,6 +67,7 @@ CREATE TABLE IF NOT EXISTS `report` (
   `date` date NOT NULL DEFAULT '1969-10-18',
   `burnt` int(11) NOT NULL,
   `estimate` int(11) NOT NULL,
+  `reason` enum('forward','initialize','estimate') NOT NULL DEFAULT 'estimate',
   PRIMARY KEY (`task_id`,`resource_id`,`date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -99,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `sprint` (
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
   PRIMARY KEY (`sprint_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 -- --------------------------------------------------------
 
@@ -116,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `task` (
   `story` text NOT NULL,
   PRIMARY KEY (`task_id`),
   KEY `sprint_id` (`sprint_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=50 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=57 ;
 
 -- --------------------------------------------------------
 
@@ -125,8 +121,4 @@ CREATE TABLE IF NOT EXISTS `task` (
 --
 DROP TABLE IF EXISTS `extended_log`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `extended_log` AS select `log`.`type` AS `type`,`log`.`details` AS `details`,`resource`.`name` AS `name`,`task`.`description` AS `description` from ((`log` left join `resource` on((`resource`.`resource_id` = `log`.`resource_id`))) join `task` on((`log`.`task_id` = `task`.`task_id`)));
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `extended_log` AS select `log`.`time` AS `time`,`log`.`type` AS `type`,`log`.`details` AS `details`,`resource`.`name` AS `name`,`task`.`description` AS `description` from ((`log` left join `resource` on((`resource`.`resource_id` = `log`.`resource_id`))) join `task` on((`log`.`task_id` = `task`.`task_id`))) order by `log`.`time` desc;
