4257cc4187ee4159b970ff18c7c3330e6374ba49
danny@havenith.net <dh.git@havenith.net>
2013-07-18 16:53:05 +0200

    Fix IE8 issues by moving to latest versions of jquery and jqueryui.
    jquery version is now 1.10.2
    jqueryui version is 1.10.3, with smoothness skin
    dataTables was patched to prevent errors when IE8 recursively calls draw on a table.

--- team_member.php
+++ team_member.php
@@ -9,11 +9,11 @@ require_once 'get_sprint_id.inc.php';
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link href="css/smoothness/jquery-ui-1.8.20.custom.css" rel="stylesheet" type="text/css"/>
+<link href="css/smoothness/jquery-ui-1.10.3.custom.min.css" rel="stylesheet" type="text/css"/>
 <link href="css/scram.css" rel="stylesheet" type="text/css"/>
 <link href="css/holygrail.css" rel="stylesheet" type="text/css"/>
-<script type="text/javascript" src="scripts/jquery-1.7.2.min.js"/></script>
-<script type="text/javascript" src="scripts/jquery-ui-1.8.20.custom.min.js"/></script>
+<script type="text/javascript" src="scripts/jquery-1.10.2.min.js"/></script>
+<script type="text/javascript" src="scripts/jquery-ui-1.10.3.custom.min.js"/></script>
 <script type="text/javascript" src="scripts/jquery.jeditable.mini.js"/></script>
 <script type="text/javascript" src="scripts/jquery.numeric.js"/></script>
 <script type="text/javascript" src="scripts/scram.js"></script>
